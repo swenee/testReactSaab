11fa430a7d48fd87a4ef13717f2b4c5b55f62a5d
unknown <swenee.hagberg@gmail.com>
2021-01-29 09:30:23 +0100

    added app skeleton

---
+++ testreactsaab/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <p className = "Title">Droppable here :)
+      </p>
+      <div className="content">
+
+      </div>
+
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
