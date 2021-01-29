0d55a4d4298894d6dda9843d920964c444614e28
unknown <swenee.hagberg@gmail.com>
2021-01-29 10:08:54 +0100

    forgot to add changes

--- testreactsaab/src/App.js
+++ testreactsaab/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
+import Droppable from "./Droppable"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
-      <p className = "Title">Droppable here :)
+      <p className = "Title">Droppable
       </p>
       <div className="content">
-
+        <Droppable />
       </div>
 
       </header>
